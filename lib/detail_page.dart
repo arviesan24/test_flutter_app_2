15cec5bb0b7f8fbf85e4c404af393e99401951d6
Romel Santoral <ctechnology24@gmail.com>
2018-10-09 00:24:03 +0800

    Import `http`

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class DetailPage extends StatefulWidget {
   @override
@@ -10,7 +11,7 @@ class _DetailPageState extends State<DetailPage> {
   var _details;
 
   _fetchData() async{
-    
+
   }
 
   @override
