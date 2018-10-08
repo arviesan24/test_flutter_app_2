2c6837d86af7ac7d04c11ffeb0ff817296917dba
Romel Santoral <ctechnology24@gmail.com>
2018-10-09 00:05:17 +0800

    remove `DetailPage` class

--- lib/main.dart
+++ lib/main.dart
@@ -79,14 +79,3 @@ class _RealWorldAppState extends State<RealWorldApp> {
   }
 }
 
-class DetailPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text('Details'),
-      ),
-      body: new Center(child: new Text('Details will show here.')),
-    );
-  }
-}
